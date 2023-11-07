2d27af4f2322c742c7d87efd19ea1e2dc4c70a51
ericyoun430 <eyoun@ucsd.edu>
2023-11-07 12:29:06 -0800

    Added simple UI

--- js/logic.js
+++ js/logic.js
@@ -1,6 +1,3 @@
-
-
-
 function getComputerChoice() {
     let randInt = Math.random();
 
@@ -50,28 +47,56 @@ let playGame = (playerSelection, computerSelection) => {
         case (playerSelection == "scissors" && computerSelection == "scissors"):
             out = `You tied!`;
             break;
-        
     }
     return out;
 }
 
 
-function game() {
-
-    let winCount = 0;
-
-    for (let i = 0; i < 5; i++) {
-        let userInput = prompt('What do you choose to do?','')
-        let computerChoice = getComputerChoice();
-        let gameOutput = playGame(userInput,computerChoice);
-        if (gameOutput.substring(0,7) == "You won") {
-            winCount++;
-        }
-    }
-
-    console.log(winCount);
-
-    let won = (winCount > 2) ? true : false;
-
-    return won;
+function game(userInput) {
+    let computerChoice = getComputerChoice();
+    let gameOutput = playGame(userInput,computerChoice);
+    return gameOutput;
 }
+
+
+let rock = document.querySelector('#rock');
+let paper = document.querySelector('#paper');
+let scissors = document.querySelector('#scissors');
+let outcomeDiv = document.querySelector('#outcome');
+let winCountDiv = document.querySelector('#winCount');
+
+let winCount = 0;
+let lossCount = 0;
+
+let moves = [rock,paper,scissors];
+
+for (let i = 0; i < moves.length; i++) {
+    let move = moves[i];
+    move.addEventListener('click',() => {
+
+        let outcome = (i === 0) ? game('rock') : (i === 1) ? 
+            game('paper') : game('scissors');
+
+        if (outcome.substring(0,7) === 'You won') {
+            winCount++;
+        } else if (outcome.substring(0,7) ==='You los') {
+            lossCount++;
+        }
+        outcomeDiv.textContent = "Outcome: " + outcome;
+        winCountDiv.textContent = "Current Score: " + winCount + "-" + lossCount;
+    
+        if (winCount >= 5 && document.querySelector('#add') === null) {
+            let winDiv = document.createElement('div');
+            winDiv.textContent = "You won the overall game";
+            winDiv.setAttribute('id','add');
+            document.querySelector('body').appendChild(winDiv);
+        }
+        
+        if (lossCount >= 5 && document.querySelector('#add') === null) {
+            let lossDiv = document.createElement('div');
+            lossDiv.textContent = "You lost the overall game";
+            lossDiv.setAttribute('id','add');
+            document.querySelector('body').appendChild(lossDiv);
+        }
+    });
+};
